ee7ffa7eb0185ac39d637ca879296448ae899ca0
Giuseppe Grant <dev.giuseppe.grant@gmail.com>
2024-01-23 22:40:16 -0500

    Folders Proyects
    - Folders projects Added.

---
+++ Easy/Challenge 1/Solutions/Solution.js
@@ -0,0 +1,10 @@
+// Solution 1 : Using prompt and concatenation
+let name = prompt("Write your name!");
+let greetings = "Greetings";
+alert(greetings + " " + name);
+console.log(greetings + " " + name);
+
+// Solution 2 : Using prompt and template String
+let name2 = prompt("Write your name!");
+alert(`Greetings ${name2}`);
+console.log(`Greetings ${name2}`);
